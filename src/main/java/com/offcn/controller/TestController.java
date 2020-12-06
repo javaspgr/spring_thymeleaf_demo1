e9e61b6c222abe5e7a750e28caa840dd313adaae
hulei <123qq.com>
2020-12-06 22:24:07 +0800

    新增判断

--- src/main/java/com/offcn/controller/TestController.java
+++ src/main/java/com/offcn/controller/TestController.java
@@ -5,8 +5,10 @@ public class TestController {
 //添加输出语句
     public  void add(){
         System.out.println("1111");
-        
-        
-        
+
+        if (true){
+            System.out.println("傻大个");
+        }
     }
+
 }
